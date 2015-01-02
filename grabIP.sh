07459ad35ce4202b1e76cfeeadf8b6064f15d5cd
quhezheng <quhezheng@126.com>
2015-01-02 23:32:56 +0800

    upload the script to grad IP web and upload to GitHub

--- grabIP.sh
+++ grabIP.sh
@@ -5,4 +5,4 @@ wget http://www.kpcn.org/tools/ip.asp -O ~/git/repos/HomeIP/ip.html
 sh -c 'date +%D_%T' >> ~/git/repos/HomeIP/ip.html
 git add ip.html
 git commit -m "update"
-git push origin master 
+git push origin master  
