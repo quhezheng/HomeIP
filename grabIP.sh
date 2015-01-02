955f415a99e64ece5f4f9c231453203f3490dad5
quhezheng <quhezheng@126.com>
2015-01-02 23:30:10 +0800

    update

---
+++ grabIP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/git/repos/HomeIP
+git pull
+wget http://www.kpcn.org/tools/ip.asp -O ~/git/repos/HomeIP/ip.html
+sh -c 'date +%D_%T' >> ~/git/repos/HomeIP/ip.html
+git add ip.html
+git commit -m "update"
+git push origin master 
