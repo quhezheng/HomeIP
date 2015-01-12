9d555bd19904d2b10cf0931a6018ee9d410c49f3
quhezheng <quhezheng@126.com>
2015-01-12 22:25:26 +0800

    hit the csdn blog

---
+++ wget_csdn.sh
@@ -0,0 +1,4 @@
+wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42609839 -O /dev/null
+wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42466843 -O /dev/null
+wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42650469 -O /dev/null
+
