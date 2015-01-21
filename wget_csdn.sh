63c5d7abdb48dcd01830c774366f36cf4c81fb7b
quhezheng <quhezheng@126.com>
2015-01-21 21:49:34 +0800

    updae csdn links

--- wget_csdn.sh
+++ wget_csdn.sh
@@ -2,4 +2,4 @@ wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/4
 wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42466843 -O /dev/null
 wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42650469 -O /dev/null
 wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42840955 -O /dev/null
-
+wget -U NoSuchBrowser/1.0  http://blog.csdn.net/crazyquhezheng/article/details/42974817 -O /dev/null
